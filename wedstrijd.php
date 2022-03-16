72c69295944f0c4fe7d4424392198d9fbb2fd279
Diego <d.lens2712@gmail.com>
2022-03-16 13:53:07 +0100

    Created wedstrijd.php. Created style.css. Created img folder. Added backarrow.png. Added content to wedstrijd.php.

---
+++ wedstrijd.php
@@ -0,0 +1,44 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <img src="img/backarrow.png" alt="back arrow">
+            <div class="title">Wedstrijd</div>
+        </div>
+        <main>
+            <div class="score">
+                1 - 0
+            </div>
+            <div class="teamVs">
+                Team 1 vs Team 2
+            </div>
+            <div class="formation">
+
+            </div>
+            <div class="formationNames">
+                <div class="formationName">Speler 1</div>
+                <div class="formationName">Speler 2</div>
+                <div class="formationName">Speler 3</div>
+                <div class="formationName">Speler 4</div>
+                <div class="formationName">Speler 5</div>
+                <div class="formationName">Speler 6</div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
