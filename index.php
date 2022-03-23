0829356806ca58454caa1069c8a424a01842316f
Diego <d.lens2712@gmail.com>
2022-03-23 11:13:06 +0100

    Renamed profielMaken.php to positieKiezen.php. Edited positieKiezen.php.

--- index.php
+++ index.php
@@ -17,6 +17,6 @@
 <body>
     <p>Tijdelijke navigatie pagina</p>
     <a href="wedstrijd.php">Wedstrijd page</a><br>
-    <a href="profielMaken.php">Profiel maken page</a><br>
+    <a href="positieKiezen.php">Profiel maken page</a><br>
 </body>
 </html>
--- positieKiezen.php
+++ positieKiezen.php
@@ -12,27 +12,26 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Kies je positie</title>
 </head>
 <body>
     <div class="wrapper">
         <div class="header">
             <a href="index.php"><img src="img/backarrow.png" alt="back arrow"></a>
-            <div class="title">Profiel maken</div>
+            <div class="title">Kies je positie</div>
         </div>
         <main>
             <form action="">
                 <div class="data-field">
-                    <img src="img/default-avatar.png" alt="Profiel foto">
-                    <label for="image">Profiel foto</label>
-                    <input type="file" name="image" id="image">
+                    <label for="position">Primaire positie</label>
+                    <select name="position" id="position">
+                        <option value="keeper">Keeper</option>
+                        <option value="aanvaller">Aanvaller</option>
+                        <option value="verdediger">Verdediger</option>
+                    </select>
                 </div>
                 <div class="data-field">
-                    <label for="name">Naam</label>
-                    <input type="text" name="name" id="name">
-                </div>
-                <div class="data-field">
-                    <label for="position">Positie</label>
+                    <label for="position">Secundaire positie</label>
                     <select name="position" id="position">
                         <option value="keeper">Keeper</option>
                         <option value="aanvaller">Aanvaller</option>
