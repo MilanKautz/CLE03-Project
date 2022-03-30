3b4c4277a478a1e9baac11329b431c8d7d0e278a
sayyid15 <sayyidbrown76@gmail.com>
2022-03-30 14:08:25 +0200

    pages

---
+++ Teams.php
@@ -0,0 +1,92 @@
+
+
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Account Edit</title>
+    <meta charset="utf-8"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <!--    <meta name="viewpart"-->
+    <!--    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>-->
+    <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" type="text/css" href="css/Sayyid.css"/>
+</head>
+
+<body>
+<div class="wrapper">
+<div class="header" >
+    <img src="img/backarrow.png" alt="back arrow">
+    <div class="team">Team </div>
+
+</div>
+    <main>
+         <h1>Je bent team 2</h1>
+
+
+
+
+<table>
+<tr>
+        <th>Spelers</th>
+        <th>Verdediger</th>
+</tr>
+    <tr>
+        <td>Speler 1</td>
+        <td>Aanvaller</td>
+</tr>
+    <tr>
+        <td>Speler 2</td>
+        <td>Doelman</td>
+</tr>
+    <tr>
+        <td>Speler 3</td>
+        <td>Middenvelder</td>
+</tr>
+    <tr>
+        <td>Speler 4</td>
+        <td>Aanvaller</td>
+</tr><tr>
+        <td>Speler 5</td>
+        <td>Verdediger</td>
+</tr><tr>
+        <td>Speler 6</td>
+        <td>Verdediger</td>
+</tr><tr>
+        <td>Speler 7</td>
+        <td>Middenvelder</td>
+</tr><tr>
+        <td>Speler 8</td>
+        <td></td>
+</tr><tr>
+        <td>Speler 9</td>
+        <td></td>
+</tr><tr>
+        <td>Speler 10</td>
+        <td></td>
+</tr><tr>
+        <td>Speler 11</td>
+        <td>Middenvelder</td>
+</tr><tr>
+        <td>Speler 12</td>
+        <td>Aanvaller</td>
+</tr><tr>
+        <td>Speler 11</td>
+        <td>Verdediger</td>
+</tr>
+
+
+</table>
+
+        <h2>Scheidsrechter:..........</h2>
+    </main>
+</div>
+</body>
+</html>
