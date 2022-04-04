1a632d718ff0ad474ff1507140d7bc1a79d8924f
Diego <d.lens2712@gmail.com>
2022-04-04 12:10:47 +0200

    Created overOns.php.

---
+++ overOns.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/diego.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="manifest" href="manifest.json">
+    <title>Over ons</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <a href="index.php"><img id="backArrow" src="img/backarrow.png" alt="back arrow"></a>
+            <div class="title">Over ons</div>
+        </header>
+        <main class="wedstrijdBeheerMain">
+
+        </main>
+    </div>
+</body>
+</html>
