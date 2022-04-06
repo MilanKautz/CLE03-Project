dd908eb5f561c83eab92ed212a0abcd7a8c915d9
sayyid15 <sayyidbrown76@gmail.com>
2022-04-06 10:27:28 +0200

    pages

---
+++ Wedsrijdoverzicht.php
@@ -0,0 +1,73 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Account Edit</title>
+    <meta charset="utf-8"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <!--    <meta name="viewpart"-->
+    <!--    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>-->
+    <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/Sayyid.css"/>
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="header" >
+        <img src="img/backarrow.png" alt="back arrow">
+        <div class="team">Wedstrijd overzicht </div>
+
+    </div>
+    <main>
+
+
+
+
+
+        <table>
+
+           <tr><td>Team1 vs Team 5</td>
+                <td>   10 april 19:00
+                    <input type="submit" class="button" value="Kijken" >
+                    <input type="submit" class="button" value="Spelen" >
+               </td>
+
+           </tr>
+
+            <tr><td>Team 15 vs Team 23</td>
+                <td>25 april 17:00
+                    <input type="submit" class="button" value="Kijken" >
+                    <input type="submit" class="button" value="Spelen" ></td>
+            </tr>
+            <tr><td>Team 7 vs Team 3</td>
+                <td>27 april 13:00
+                    <input type="submit" class="button" value="Kijken" >
+                    <input type="submit" class="button" value="Spelen" ></td>
+            </tr>
+            <tr><td>Team Zuid vs Team Noord</td>
+                <td> 10 mei 11:00
+                    <input type="submit" class="button" value="Kijken" >
+                    <input type="submit" class="button" value="Spelen" ></td>
+            </tr>
+            <tr><td>Team Oost vs Team West</td>
+                <td>18 mei 18:00
+                    <input type="submit" class="button" value="Kijken" >
+                    <input type="submit" class="button" value="Spelen" ></td>
+            </tr>
+
+
+        </table>
+
+
+    </main>
+</div>
+</body>
+</html>
